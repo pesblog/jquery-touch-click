2cd8e1975e7a931a75ec93f6e3d5e776230efebf
Robert Lynch <rcl@agilepro.com>
2013-03-29 16:30:16 -0700

    Added movement forgiveness and other small fixes

--- src/jquery.touchclick.js
+++ src/jquery.touchclick.js
@@ -13,8 +13,8 @@
 
     var pluginName = 'touchClick';
     var defaults = {
-    	className: 'active',
-		moveForgiveness: 15, //distance in pixels that the touch/mouse can move and still be considered a click
+        className: 'active',
+        moveForgiveness: 15, //distance in pixels that the touch/mouse can move and still be considered a click
         callback: function(){} //removed console.log from default callback to avoid potential issue with versions of IE < 8.
     };
 
@@ -24,22 +24,22 @@
     var touchendEvent = isTouch ? 'touchend.'+pluginName : 'mouseup.'+pluginName;
 
     function Plugin( element, className, callback, context ) {
-	    this.element = element;
+        this.element = element;
         if (typeof className === 'function'){
             this.options = {};
             this.options.className = defaults.className;
             this.options.callback = className;
-			this.options.context = callback;
+            this.options.context = callback;
         } else {
             this.options = {
                 className: className || defaults.className,
                 callback: callback || defaults.callback,
-				context: context
+                context: context
             };
         }
-	    this._defaults = defaults;
-	    this._name = pluginName;
-	    this.init();
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
     }
 
     Plugin.prototype.init = function () {
@@ -47,34 +47,34 @@
         var $element = $(self.element);
         var className = self.options.className;
         var callback = self.options.callback;
-		var context = self.options.context || self.element;
-		var moveForgiveness = self._defaults.moveForgiveness;
+        var context = self.options.context || self.element;
+        var moveForgiveness = self._defaults.moveForgiveness;
         $element
             .bind(touchstartEvent, function(e) {
-				if(isTouch || e.which === 1) //only react to left click
-				{
-					this.touchClickStart = true;
-					//Get the position of the touchStart
-					if(isTouch)	{
-						this.pos = {pageX: e.originalEvent.touches[0].pageX,pageY: e.originalEvent.touches[0].pageY};
-					}else{
-						this.pos = {pageX:e.pageX,pageY:e.pageY};
-					}
-					e.preventDefault();
-					$element.addClass( className );
-				}
+                if(isTouch || e.which === 1) //only react to left click
+                {
+                    this.touchClickStart = true;
+                    //Get the position of the touchStart
+                    if(isTouch)	{
+                        this.pos = {pageX: e.originalEvent.touches[0].pageX,pageY: e.originalEvent.touches[0].pageY};
+                    }else{
+                        this.pos = {pageX:e.pageX,pageY:e.pageY};
+                    }
+                    e.preventDefault();
+                    $element.addClass( className );
+                }
             })
             .bind(touchmoveEvent, function(e) {
                 if ( this.touchClickStart ) {
-					//get the new position. Note: only checks the first finger to touch the screen.
-					var currentPosition = e.originalEvent.changedTouches ? e.originalEvent.changedTouches[0] : e;
-					window.lastTouch = e;
-					//if new position is outside of the moveForgiveness "box"...
-					if(currentPosition.pageX > this.pos.pageX + moveForgiveness || currentPosition.pageX < this.pos.pageX - moveForgiveness || currentPosition.pageY > this.pos.pageY + moveForgiveness || currentPosition.pageY < this.pos.pageY - moveForgiveness){
-						//...cancel the potential touchclick action
-						this.touchClickStart = undefined;
-						$element.removeClass( className );
-					}
+                    //get the new position. Note: only checks the first finger to touch the screen.
+                    var currentPosition = e.originalEvent.changedTouches ? e.originalEvent.changedTouches[0] : e;
+                    window.lastTouch = e;
+                    //if new position is outside of the moveForgiveness "box"...
+                    if(currentPosition.pageX > this.pos.pageX + moveForgiveness || currentPosition.pageX < this.pos.pageX - moveForgiveness || currentPosition.pageY > this.pos.pageY + moveForgiveness || currentPosition.pageY < this.pos.pageY - moveForgiveness){
+                        //...cancel the potential touchclick action
+                        this.touchClickStart = undefined;
+                        $element.removeClass( className );
+                    }
                 }
             })
             .bind(touchendEvent, function(e) {
@@ -87,21 +87,21 @@
                     }, 0);
                 }
             });
-		//allow touchclick to be triggered with $('#elem').touchClick();
-	    this.element.touchClick = function()
-	    {
-			var dom = context || this.element;
-			$.proxy(callback, dom)();
-	    }
+        //allow touchclick to be triggered with $('#elem').touchClick();
+        this.element.touchClick = function()
+        {
+            var dom = context || this.element;
+            $.proxy(callback, dom)();
+        }
     };
 
     $.fn[pluginName] = function ( className, callback ) {
-	    return this.each(function () {
-	        if (!$.data(this, 'plugin_' + pluginName)) {
-		        $.data(this, 'plugin_' + pluginName,
-		               new Plugin( this, className, callback ));
-	        }
-	    });
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                       new Plugin( this, className, callback ));
+            }
+        });
     };
 
 })( jQuery, window );
